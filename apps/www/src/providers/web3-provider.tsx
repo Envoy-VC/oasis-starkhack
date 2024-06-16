ffc83879d7d9a961c52fdfa728b461a0544661d6
Vedant Chainani <vedantchainani1084@gmail.com>
2024-06-16 12:25:01 +0530

    chore: sync starknet-react and dynamic

--- apps/www/src/providers/web3-provider.tsx
+++ apps/www/src/providers/web3-provider.tsx
@@ -1,20 +1,98 @@
 'use client';
 
-import type { PropsWithChildren } from 'react';
+import { type PropsWithChildren, useEffect } from 'react';
 
-import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core';
+import {
+  DynamicContextProvider,
+  useDynamicContext,
+} from '@dynamic-labs/sdk-react-core';
 import { StarknetWalletConnectors } from '@dynamic-labs/starknet';
+import { sepolia } from '@starknet-react/chains';
+import {
+  StarknetConfig,
+  argent,
+  braavos,
+  publicProvider,
+  useConnect,
+  useDisconnect,
+  voyager,
+} from '@starknet-react/core';
 import { env } from '~/env';
 
 export const Web3Provider = ({ children }: PropsWithChildren) => {
+  return (
+    <StarknetConfig
+      chains={[sepolia]}
+      explorer={voyager}
+      provider={publicProvider()}
+    >
+      <DynamicProvider>
+        <SyncDynamicStarknet>{children}</SyncDynamicStarknet>
+      </DynamicProvider>
+    </StarknetConfig>
+  );
+};
+
+const DynamicProvider = ({ children }: PropsWithChildren) => {
+  const { connect } = useConnect();
+  const { disconnect } = useDisconnect();
+
+  const syncConnect = (key: string) => {
+    switch (key) {
+      case 'argent':
+        connect({ connector: argent() });
+        break;
+      case 'braavos':
+        connect({ connector: braavos() });
+        break;
+    }
+  };
+
   return (
     <DynamicContextProvider
       settings={{
         walletConnectors: [StarknetWalletConnectors],
         environmentId: env.NEXT_PUBLIC_DYNAMIC_ENV_ID,
+        events: {
+          onAuthSuccess: ({ primaryWallet }) => {
+            if (!primaryWallet) return;
+            const id = primaryWallet.key.toLowerCase();
+            syncConnect(id);
+          },
+          onLogout: () => {
+            disconnect();
+          },
+        },
       }}
     >
       {children}
     </DynamicContextProvider>
   );
+};
+
+const SyncDynamicStarknet = ({ children }: PropsWithChildren) => {
+  const { isAuthenticated, walletConnector } = useDynamicContext();
+  const { connectAsync } = useConnect();
+
+  useEffect(() => {
+    const syncConnect = async (key: string) => {
+      switch (key) {
+        case 'argentx':
+          await connectAsync({ connector: argent() });
+          break;
+        case 'braavos':
+          await connectAsync({ connector: braavos() });
+          break;
+      }
+    };
+    if (isAuthenticated) {
+      const id = walletConnector?.key.toLowerCase();
+      console.log(id);
+      if (id) {
+        void syncConnect(id);
+      }
+    }
+  }, [connectAsync, isAuthenticated, walletConnector?.key]);
+
+  return children;
 };
