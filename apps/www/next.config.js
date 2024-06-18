68732c81ad2e7adc11dd0bf68d0f62ff8a868f80
Vedant Chainani <vedantchainani1084@gmail.com>
2024-06-18 18:07:58 +0530

    chore: add liveblocks config

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -2,9 +2,6 @@ await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
-  // experimental: {
-  //   reactCompiler: true,
-  // },
   webpack: (config) => {
     config.experiments = { asyncWebAssembly: true, layers: true };
     return config;
--- apps/www/src/env.js
+++ apps/www/src/env.js
@@ -8,9 +8,11 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_DYNAMIC_ENV_ID: z.string().min(1),
+    NEXT_PUBLIC_LIVEBLOCKS_API_KEY: z.string().min(1),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_DYNAMIC_ENV_ID: process.env.NEXT_PUBLIC_DYNAMIC_ENV_ID,
+    NEXT_PUBLIC_LIVEBLOCKS_API_KEY: process.env.NEXT_PUBLIC_LIVEBLOCKS_API_KEY,
   },
   skipValidation: Boolean(process.env.SKIP_ENV_VALIDATION),
   emptyStringAsUndefined: true,
