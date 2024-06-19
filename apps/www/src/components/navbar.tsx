a7c46c79d308d043acc76a5afb64c939759f9f6d
Vedant Chainani <vedantchainani1084@gmail.com>
2024-06-19 19:25:13 +0530

    chore: update components

--- apps/www/src/components/navbar.tsx
+++ apps/www/src/components/navbar.tsx
@@ -1,20 +1,13 @@
-import dynamic from 'next/dynamic';
-
 import React from 'react';
 
-import { ConnectButton } from './connect-button';
-
-const InitializeDojo = dynamic(async () => await import('../initialize-dojo'), {
-  ssr: false,
-});
+import { DynamicWidget } from '@dynamic-labs/sdk-react-core';
 
 export const Navbar = () => {
   return (
     <div className='h-[6dvh] w-full border'>
-      <InitializeDojo />
       <div className='mx-auto flex h-full max-w-screen-xl items-center justify-between px-4'>
         <div className='text-xl font-semibold'>Web3 Starter</div>
-        <ConnectButton />
+        <DynamicWidget variant='modal' />
       </div>
     </div>
   );
--- apps/www/src/components/room.tsx
+++ apps/www/src/components/room.tsx
@@ -1,31 +1,25 @@
-'use client';
-
 import type { PropsWithChildren } from 'react';
 
 import { LiveMap } from '@liveblocks/client';
 import {
-  ClientSideSuspense,
-  LiveblocksProvider,
-  RoomProvider,
+	ClientSideSuspense,
+	LiveblocksProvider,
+	RoomProvider,
 } from '@liveblocks/react/suspense';
-import { env } from '~/env';
 
 export const Room = ({ children }: PropsWithChildren) => {
-  return (
-    <LiveblocksProvider
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- safe
-      publicApiKey={env.NEXT_PUBLIC_LIVEBLOCKS_API_KEY as string}
-    >
-      <RoomProvider
-        id='my-room'
-        initialStorage={{
-          layers: new LiveMap(),
-        }}
-      >
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
-          {children}
-        </ClientSideSuspense>
-      </RoomProvider>
-    </LiveblocksProvider>
-  );
+	return (
+		<LiveblocksProvider
+			publicApiKey={import.meta.env.VITE_LIVEBLOCKS_API_KEY as string}>
+			<RoomProvider
+				id='my-room'
+				initialStorage={{
+					layers: new LiveMap(),
+				}}>
+				<ClientSideSuspense fallback={<div>Loading…</div>}>
+					{children}
+				</ClientSideSuspense>
+			</RoomProvider>
+		</LiveblocksProvider>
+	);
 };
