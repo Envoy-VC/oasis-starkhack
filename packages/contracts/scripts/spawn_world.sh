a9d3268d791b2eb596b1d7691c4418b25d972b7d
Vedant Chainani <vedantchainani1084@gmail.com>
2024-06-19 19:22:37 +0530

    chore: update scripts

--- packages/contracts/scripts/spawn_world.sh
+++ packages/contracts/scripts/spawn_world.sh
@@ -7,4 +7,4 @@ export RPC_URL="http://localhost:5050";
 # export WORLD_ADDRESS=$(cat ./manifests/dev/manifest.json | jq -r '.world.address')
 
 # sozo execute --world <WORLD_ADDRESS> <CONTRACT> <ENTRYPOINT>
-sozo execute --world 0x6fa7fa3957de2627d47b860075884c01f45c62461579f4881eee6ab5a9e87bd starksketch::systems::actions::actions initialize --wait
+sozo execute --world 0x6fa7fa3957de2627d47b860075884c01f45c62461579f4881eee6ab5a9e87bd starksketch::systems::actions::actions spawn_game -c 0x1,0x2 --wait
