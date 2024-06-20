821c2bf7afd8619255af9c7f4cdd88d3b58a68a9
Vedant Chainani <vedantchainani1084@gmail.com>
2024-06-20 20:29:23 +0530

    chore: add game page gating logic

--- apps/www/src/pages/game.tsx
+++ apps/www/src/pages/game.tsx
@@ -1,16 +1,33 @@
 import { useSearchParams } from 'react-router-dom';
 
+import { useDojo } from '~/lib/hooks';
+
+import { getComponentValue } from '@dojoengine/recs';
+import { getEntityIdFromKeys } from '@dojoengine/utils';
 import { Room, Whiteboard } from '~/components';
 
 const GamePage = () => {
   const [searchParams] = useSearchParams();
+  const {
+    burnerAccount: { account },
+    clientComponents: { Player },
+  } = useDojo();
 
   const gameID = searchParams.get('id');
 
-  // TODO: Conditional check for gameID and player address
-
   if (!gameID) {
     return <div>Game ID is required</div>;
+  }
+
+  const playerEntity = getEntityIdFromKeys([
+    BigInt(account.address),
+    BigInt(`0x${Buffer.from(gameID).toString('hex')}`),
+  ]);
+
+  const player = getComponentValue(Player, playerEntity);
+
+  if (!player) {
+    return <div>Player not found</div>;
   }
 
   return (
